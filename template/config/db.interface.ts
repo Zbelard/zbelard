e209bff40d8125192567f3597f340229fcc0c9db
AliBakerSartawi <alisartawi@live.com>
2025-01-11 10:29:50 +0300

    simplify db connection config

--- template/config/db.interface.ts
+++ template/config/db.interface.ts
@@ -1,18 +1,6 @@
-import { NumberStringOrLiteral, StringOrLiteral } from '../type-utils/index.ts';
-
 export interface ZbelardDatabaseConfig {
   persistable:
     | { driver: 'postgres'; url: string }
-    | {
-        url?: never;
-        driver: 'postgres';
-        host?: StringOrLiteral<'127.0.0.1'>;
-        port?: NumberStringOrLiteral<'5432'>;
-        database?: StringOrLiteral<'zbelard'>;
-        username?: StringOrLiteral<'user'>;
-        password?: StringOrLiteral<'pass'>;
-        sslmode?: 'prefer' | 'require';
-      };
 
   /**
    * Let's not call this cache, because you might use Redis to share global state (like a gangster)
