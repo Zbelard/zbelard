7bb45eb0f489c090234b76ac34bd843095284239
AliBakerSartawi <alisartawi@live.com>
2025-01-15 10:36:12 +0300

    add kysely db client

---
+++ db/db.ts
@@ -0,0 +1,22 @@
+import type { DB } from "./db.d.ts";
+// @ts-types="@types/pg"
+import pg from "pg";
+import { Kysely, ParseJSONResultsPlugin, PostgresDialect } from "kysely";
+// @ts-types="@types/pg-cursor"
+import Cursor from "pg-cursor";
+
+const connectionString = Deno.env.get("DATABASE_URL") ||
+  "postgres://user:pass@localhost:5432/zbelard";
+
+const pool = new pg.Pool({ connectionString: connectionString });
+
+const dialect = new PostgresDialect({
+  pool,
+  // cursor is needed for streaming results from the database
+  cursor: Cursor,
+});
+
+export const db = new Kysely<DB>({
+  dialect,
+  plugins: [new ParseJSONResultsPlugin()],
+});
