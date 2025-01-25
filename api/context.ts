9945257d1919aff02802297a9a58ab5e492bf993
AliBakerSartawi <alisartawi@live.com>
2025-01-25 15:49:26 +0300

    comment out auth for now

--- api/context.ts
+++ api/context.ts
@@ -1,16 +1,18 @@
 import { db } from "@zbelard/db";
 import { Repos } from "@zbelard/core/repos";
-import { getSession } from "next-auth/react";
+// import { getSession } from "next-auth/react";
 import type { NodeHTTPCreateContextFnOptions } from "@trpc/server/adapters/node-http";
 import type { IncomingMessage, ServerResponse } from "node:http";
+import type { Session } from "next-auth";
 
 export const createContext = async (
-  opts: NodeHTTPCreateContextFnOptions<
+  _opts: NodeHTTPCreateContextFnOptions<
     IncomingMessage,
     ServerResponse<IncomingMessage>
   >,
 ) => {
-  const session = await getSession({ req: opts.req });
+  // const session = await getSession({ req: opts.req });
+  const session: Session | null = null;
 
   return {
     session,
